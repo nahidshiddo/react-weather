844a674612ffadcabb5062c2523ac114ebdb3345
Nahid Shiddo <100500227+nahidshiddo@users.noreply.github.com>
2022-06-06 23:23:00 -0400

    Added starter code for project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import Weather from "./Weather"
+import Loader from "react-loader-spinner";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
 
-      <h1>Hello from React</h1>
+      <h1>Weather App</h1>
       <Weather city ="Paris" />
       
       </header>
+
+        <Loader
+        type="Puff"
+        color="#00BFFF"
+        height={100}
+        width={100}
+        timeout={3000}
+        />
+
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,27 +1,66 @@
-import React from "react";
-import axios from 'axios';
-// import Loader from "react-loader-spinner";
+import React, { useState } from "react";
+import axios from "axios";
 
 
-export default function Weather(props){
-    function handleResponse(response){
-        alert(`The weather in ${response.data.name} is ${response.data.main.temp}°C`);
-    }
 
+export default function WeatherSearch() {
+  const [city, SetCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description
+    });
+  }
+
+  function handleSumbit(event) {
+    event.preventDefault();
     let apiKey = "601404a482223c629a0622a7d06f6491";
     let units = "metric";
-     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=${units}`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(displayWeather);
+  }
 
-     axios.get(apiUrl).then(handleResponse);
+  function updateCity(event) {
+    SetCity(event.target.value);
+  }
 
-    return 
+  let form = (
+    <form onSubmit={handleSumbit}>
+      <input type="search" placeholder="Enter a city.." onChange={updateCity} />
+      <button type="submit">Search</button>
+    </form>
+  );
 
-        // <Loader
-        // type="Puff"
-        // color="#00BFFF"
-        // height={100}
-        // width={100}
-        // timeout={3000}
-        // />
-    
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature:{Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity:{weather.humidity}%</li>
+          <li>Wind:{weather.wind}km/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
+
+
+
+
+
+
+
